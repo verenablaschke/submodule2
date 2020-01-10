d250e86e2db8f2eb789c3e3c34077c5293e86192
Verena Blaschke <verena.blaschke@gmx.de>
2020-01-10 15:39:25 +0100

    imports between submodules

--- src/main/java/de/tuebingen/sfs/additionalsubmodule/Submodule2.java
+++ src/main/java/de/tuebingen/sfs/additionalsubmodule/Submodule2.java
@@ -1,6 +1,13 @@
 package de.tuebingen.sfs.additionalsubmodule;
 
+import de.tuebingen.sfs.submodulefromroot.SubmoduleFromRoot;
+
 public class Submodule2 {
+
+	public static void main(String[] args) {
+		greet();
+		SubmoduleFromRoot.greet();
+	}
 
 	public static void greet() {
 		System.out.println("Submodule2");
